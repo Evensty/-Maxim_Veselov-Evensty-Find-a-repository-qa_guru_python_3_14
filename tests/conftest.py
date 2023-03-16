095901ae986fc3f0428c58cd3647e3cb8542b4b5
evensty <maxim.veselov11@gmail.com>
2023-03-16 22:20:30 +0300

    some fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,41 +5,49 @@ from dotenv import load_dotenv
 from selene.support.conditions import have
 from selene.support.shared import browser
 
-from config import Hosts
 from framework.demoqa_with_env import DemoQaWithEnv
-from utils.base_session import BaseSession
+
 
 load_dotenv()
 
 
 def pytest_addoption(parser):
-    parser.addoption("--env")
-
-
-@pytest.fixture(scope="session")
-def env(request):
-    return request.config.getoption("--env")
+    parser.addoption("--env", default="prod")
 
 
 @pytest.fixture(scope='session')
-def demoshop(env):
-    return DemoQaWithEnv(env).demoqa
+def demoshop(request):
+    env = request.config.getoption("--env")
+    return DemoQaWithEnv(env)
 
 
 @pytest.fixture(scope='session')
-def reqres(env):
+def reqres(request):
+    env = request.config.getoption("--env")
     return DemoQaWithEnv(env).reqres
 
 
-@pytest.fixture(autouse=True)
-def window_size():
+@pytest.fixture(scope='session')
+def cookie(demoshop):
+    response = demoshop.login(os.getenv("LOGIN"), os.getenv("PASSWORD"))
+    authorization_cookie = response.cookies.get("NOPCOMMERCE.AUTH")
+    return authorization_cookie
+
+
+@pytest.fixture(scope='function')
+def demoshop_session(demoshop, cookie):
+    browser.config.base_url = demoshop.demoqa.url
     browser.config.window_width = 1920
     browser.config.window_height = 1080
+    browser.open("/Themes/DefaultClean/Content/images/logo.png")
+    browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": cookie})
+    yield browser
+    browser.quit()
 
 
 @pytest.fixture()
 def clean_cart():
-    yield
+    yield demoshop_session
     browser.open('https://demowebshop.tricentis.com/cart')
     for checkbox in browser.elements('[name="removefromcart"]'):
         checkbox.click()
@@ -49,7 +57,7 @@ def clean_cart():
 
 @pytest.fixture()
 def clean_wishlist():
-    yield
+    yield demoshop_session
     browser.open('https://demowebshop.tricentis.com/wishlist')
     for checkbox in browser.elements('[name="removefromcart"]'):
         checkbox.click()
@@ -59,7 +67,7 @@ def clean_wishlist():
 
 @pytest.fixture()
 def clear_compare_list():
-    yield
+    yield demoshop_session
     browser.element('.clear-list').click()
     browser.element('.page-body').should(have.text('You have no items to compare.'))
 
--- tests/test_demowebshop.py
+++ tests/test_demowebshop.py
@@ -1,163 +1,67 @@
 import os
 
-import requests
 from faker import Faker
-
 from allure_commons._allure import step
 from selene import have
 from selene.support.conditions import be
 from selene.support.shared import browser
-from dotenv import load_dotenv
-
-from framework.demoqa import DemoQA
-from framework.demoqa_with_env import DemoQaWithEnv
-from utils.base_session import BaseSession
 
 fake = Faker()
 
 
-load_dotenv()
-
-API_URL = os.getenv('API_URL')
-print(f"API_URL={API_URL}")
-WEB_URL = os.getenv("WEB_URL")
-print(f"WEB_URL={WEB_URL}")
-login = os.getenv('LOGIN')
-password = os.getenv('PASSWORD')
-
-browser.config.base_url = WEB_URL
-
-
-def test_login():
-    """Successful authorization to some demowebshop (UI)"""
-    with step("Open login page"):
-        browser.open("/login")
-
-    with step("Fill login form"):
-        browser.element("#Email").send_keys(login)
-        browser.element("#Password").send_keys(password).press_enter()
-
-    with step("Verify successful authorization"):
-        browser.element(".account").should(have.text(login))
-
-
-def test_login_though_api():
-    response = requests.post(f"{API_URL}/login", json={"Email": "demo_webshop@test.com", "Password": "123123"}, allow_redirects=False)
-    authorization_cookie = response.cookies.get("NOPCOMMERCE.AUTH")
-
-    browser.open("/Themes/DefaultClean/Content/images/logo.png")
-
-    browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": authorization_cookie})
-    browser.open("")
-
-    with step("Verify successful authorization"):
-        browser.element(".account").should(have.text(login))
-
-
-def test_login_though_api_with_base_session():
-    demoshop = BaseSession(API_URL)
-    response = demoshop.post("/login", json={"Email": "demo_webshop@test.com", "Password": "123123"}, allow_redirects=False)
-    authorization_cookie = response.cookies.get("NOPCOMMERCE.AUTH")
-
-    browser.open("/Themes/DefaultClean/Content/images/logo.png")
-
-    browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": authorization_cookie})
-    browser.open("")
-
-    with step("Verify successful authorization"):
-        browser.element(".account").should(have.text(login))
-
-
-def test_login_though_api_with_base_session_fixture(demoshop):
-    response = demoshop.post("/login", json={"Email": "demo_webshop@test.com", "Password": "123123"}, allow_redirects=False)
-    authorization_cookie = response.cookies.get("NOPCOMMERCE.AUTH")
-
-    browser.open("/Themes/DefaultClean/Content/images/logo.png")
-
-    browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": authorization_cookie})
-    browser.open("")
-
-    with step("Verify successful authorization"):
-        browser.element(".account").should(have.text(login))
-
-
-def test_check_cart_quantity(demoshop, clean_cart):
-    response = demoshop.post("/login", json={"Email": "demo_webshop@test.com", "Password": "123123"}, allow_redirects=False)
-    authorization_cookie = response.cookies.get("NOPCOMMERCE.AUTH")
-    browser.open("/Themes/DefaultClean/Content/images/logo.png")
-    browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": authorization_cookie})
-    demoshop.post('/addproducttocart/catalog/13/1/1')
-    demoshop.post('/addproducttocart/catalog/22/1/1')
-    demoshop.post('/addproducttocart/catalog/45/1/1')
-    browser.open("")
-    browser.element('.ico-cart .cart-label').click()
+def test_check_cart_quantity(demoshop, demoshop_session, clean_cart):
+    demoshop_session.open("")
+    demoshop.demoqa.post('/addproducttocart/catalog/13/1/1')
+    demoshop.demoqa.post('/addproducttocart/catalog/22/1/1')
+    demoshop.demoqa.post('/addproducttocart/catalog/45/1/1')
+    demoshop_session.element('.ico-cart .cart-label').click()
     with step('check cart size'):
-        browser.element('.cart-label~.cart-qty').should(have.text('(3)'))
+        demoshop_session.element('.cart-label~.cart-qty').should(have.text('(3)'))
 
 
-def test_gift_cards_match(demoshop, clean_cart):
-    response = demoshop.post("/login", json={"Email": "demo_webshop@test.com", "Password": "123123"},
-                             allow_redirects=False)
-    authorization_cookie = response.cookies.get("NOPCOMMERCE.AUTH")
-    browser.open("/Themes/DefaultClean/Content/images/logo.png")
-    browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": authorization_cookie})
-    browser.open('https://demowebshop.tricentis.com/gift-cards')
+def test_gift_cards_match(demoshop, demoshop_session, clean_cart):
+    demoshop_session.open('https://demowebshop.tricentis.com/gift-cards')
     recipient_info = {
         "name": fake.first_name(),
         "email": fake.email()
     }
 
     def fill_recipient_info():
-        browser.element('.recipient-name').type(recipient_info["name"])
-        browser.element(".recipient-email").type(recipient_info["email"])
-        browser.element('[id|=add-to-cart-button][type="button"]').click()
+        demoshop_session.element('.recipient-name').type(recipient_info["name"])
+        demoshop_session.element(".recipient-email").type(recipient_info["email"])
+        demoshop_session.element('[id|=add-to-cart-button][type="button"]').click()
 
-    browser.element('.product-title>[href="/25-virtual-gift-card"]').click()
+    demoshop_session.element('.product-title>[href="/25-virtual-gift-card"]').click()
     fill_recipient_info()
-    response = demoshop.get('/cart')
+    response = demoshop.demoqa.get('/cart')
     with step('Check recipient info'):
         assert recipient_info['name'], recipient_info['email'] in response.text
-        browser.element('.ico-cart .cart-label').click()
+        demoshop_session.element('.ico-cart .cart-label').click()
 
 
-def test_books_match(demoshop, clean_cart):
-    response = demoshop.post("/login", json={"Email": "demo_webshop@test.com", "Password": "123123"},
-                             allow_redirects=False)
-    authorization_cookie = response.cookies.get("NOPCOMMERCE.AUTH")
-    browser.open("/Themes/DefaultClean/Content/images/logo.png")
-    browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": authorization_cookie})
-    browser.open('https://demowebshop.tricentis.com/books')
+def test_books_match(demoshop, demoshop_session, clean_cart):
+    demoshop_session.open('https://demowebshop.tricentis.com/books')
     for book in browser.elements('[type = "button"][value = "Add to cart"]'):
         book.click()
         browser.wait_until(book.should(be.clickable))
-    response = demoshop.get('/cart')
+    response = demoshop.demoqa.get('/cart')
     with step('check total price'):
         assert '44.00' in response.text
 
 
-def test_add_digital_downloads_to_wishlist(demoshop, clean_wishlist):
-    response = demoshop.post("/login", json={"Email": "demo_webshop@test.com", "Password": "123123"},
-                             allow_redirects=False)
-    authorization_cookie = response.cookies.get("NOPCOMMERCE.AUTH")
-    browser.open("/Themes/DefaultClean/Content/images/logo.png")
-    browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": authorization_cookie})
-    demoshop.post('/addproducttocart/details/53/2')
-    demoshop.post('/addproducttocart/details/51/2')
-    demoshop.post('/addproducttocart/details/52/2')
-    browser.open("")
+def test_add_digital_downloads_to_wishlist(demoshop, demoshop_session, clean_wishlist):
+    demoshop_session.open("")
+    demoshop.demoqa.post('/addproducttocart/details/53/2')
+    demoshop.demoqa.post('/addproducttocart/details/51/2')
+    demoshop.demoqa.post('/addproducttocart/details/52/2')
     browser.element('#topcartlink~li .ico-wishlist').click()
     with step('check wishlist content'):
         browser.element('.share-link').should(be.existing).click()
         browser.all('.product>[href]').should(have.texts('3rd Album', 'Music 2', 'Music 2'))
 
 
-def test_compare_desktop_pc(demoshop, clear_compare_list):
-    response = demoshop.post("/login", json={"Email": "demo_webshop@test.com", "Password": "123123"},
-                             allow_redirects=False)
-    authorization_cookie = response.cookies.get("NOPCOMMERCE.AUTH")
-    browser.open("/Themes/DefaultClean/Content/images/logo.png")
-    browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": authorization_cookie})
-    browser.open('https://demowebshop.tricentis.com/desktops')
+def test_compare_desktop_pc(demoshop, demoshop_session, clear_compare_list):
+    demoshop_session.open('https://demowebshop.tricentis.com/desktops')
     browser.element('.product-title>[href="/build-your-cheap-own-computer"]').click()
     browser.element('[type="radio"][value="65"]').click()
     browser.element('[type="radio"][value="55"]').click()
@@ -172,33 +76,6 @@ def test_compare_desktop_pc(demoshop, clear_compare_list):
         assert os.path.getsize('compare.png') != 0
     os.remove('compare.png')
 
-
-def test_add_one_product_to_cart_authorized(demoshop):
-    """Тест с прокидыванием сессии во фреймворк"""
-    with step("Авторизоваться"):
-        demoqa = DemoQA(demoshop)
-        demoqa.authorization_cookie = demoqa.login(email=login, password=password)
-
-    with step("Добавление товара в корзину"):
-        result = demoqa.add_to_cart(cookies=demoqa.authorization_cookie)
-
-    with step("Товар добавлен в корзину"):
-        assert result.status_code == 200
-        assert 'The product has been added to your' in result.json()["message"]
-
-
-def test_add_one_product_to_cart_authorized_with_env(env):
-    """Тест с прокидыванием окружения во фреймворк"""
-    with step("Авторизоваться"):
-        demoqa = DemoQaWithEnv(env)
-        demoqa.authorization_cookie = demoqa.login(email=login, password=password)
-
-    with step("Добавление товара в корзину"):
-        result = demoqa.add_to_cart(cookies=demoqa.authorization_cookie)
-
-    with step("Товар добавлен в корзину"):
-        assert result.status_code == 200
-        assert 'The product has been added to your' in result.json()["message"]
 
 
 
